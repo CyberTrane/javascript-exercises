686db8f35fa5624d905322ce24d935723d90011a
CyberTrane <58540182+CyberTrane@users.noreply.github.com>
2023-02-10 01:04:02 -0500

    Completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const arr = string.split();
+    const length = string.length;
+    let newString = '';
 
+    for (let i = length - 1; i >= 0; i--) {
+        newString += string[i];
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
