154416c7caf3c26e51e87d0edcd7a1deaf2ba013
CyberTrane <58540182+CyberTrane@users.noreply.github.com>
2023-02-15 01:51:23 -0500

    Completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const initial = 0;
+    const person = people.reduce(checkAge);
+    return person;
 
+    function checkAge(person1, person2) {
+        const d = new Date();
+        const currentYear = d.getFullYear();
+        let age1;
+        let age2;
+
+        if (!('yearOfDeath' in person1)) {
+            age1 = currentYear - person1.yearOfBirth;
+        } else {
+            age1 = person1.yearOfDeath - person1.yearOfBirth;
+        }
+        
+        if (!('yearOfDeath' in person2)) {
+            age2 = currentYear - person2.yearOfBirth;
+        } else {
+            age2 = person2.yearOfDeath - person2.yearOfBirth;
+        }
+
+        if (age1 > age2) {
+            return person1;
+        } else {
+            return person2;
+        }
+    }
 };
 
 // Do not edit below this line
