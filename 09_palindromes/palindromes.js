ff15d08621232a6e5b597558091c830bc41c05d5
CyberTrane <58540182+CyberTrane@users.noreply.github.com>
2023-02-14 21:08:09 -0500

    Completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    let newString = '';
+    let arr = [];
+    for (let i = 0; i < string.length; i++) {
+        if (string.charCodeAt(i) >= 65) {
+            arr.push(string.charAt(i));
+            newString += string.charAt(i);
+        }
+    }
 
+    let reverseString = '';
+    const reverseArr = arr.reverse();
+    for (let i = 0; i < reverseArr.length; i++) {
+        reverseString += reverseArr[i];
+    }
+
+    if (newString === reverseString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
